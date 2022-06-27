fc7aabadc1776005b5fa894b4dab061f5db90258
Udi <udigeri@gmail.com>
2022-06-27 15:02:06 +0200

    adjusted javascript

--- inout/js/parking.js
+++ inout/js/parking.js
@@ -1,24 +1,22 @@
-function show_p1() {
-  document.getElementById("p1").style.display = "block";
+function hide_parking() {
+  document.getElementById("p1").style.display = "none";
   document.getElementById("p2").style.display = "none";
   document.getElementById("p3").style.display = "none";
   document.getElementById("p4").style.display = "none";
+}
+function show_p1() {
+  hide_parking();
+  document.getElementById("p1").style.display = "block";
 }
 function show_p2() {
-  document.getElementById("p1").style.display = "none";
+  hide_parking();
   document.getElementById("p2").style.display = "block";
-  document.getElementById("p3").style.display = "none";
-  document.getElementById("p4").style.display = "none";
 }
 function show_p3() {
-  document.getElementById("p1").style.display = "none";
-  document.getElementById("p2").style.display = "none";
+  hide_parking();
   document.getElementById("p3").style.display = "block";
-  document.getElementById("p4").style.display = "none";
 }
 function show_p4() {
-  document.getElementById("p1").style.display = "none";
-  document.getElementById("p2").style.display = "none";
-  document.getElementById("p3").style.display = "none";
+  hide_parking();
   document.getElementById("p4").style.display = "block";
 }
